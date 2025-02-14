c92908a007673740642b9b22b73f5c3ef9479f1e
Abhishek Gawade <abhishekgawade227@gmail.com>
2025-02-14 12:24:38 +0530

    log off and routeing

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -16,3 +16,30 @@ export interface IAPIRESPONSE{
         data: any,
       
 }
+
+export class LoggedUser{
+  userId: number =0
+  userName: string = ''
+  emailId: string = ''
+  fullName: string =''
+  role: string =''
+  createdDate: string=''
+  password: string=''
+  projectName: string=''
+  refreshToken: string=''
+  refreshTokenExpiryTime: string=''
+
+  constructor() {
+  this.userId =0
+  this.userName = ''
+  this.emailId = ''
+  this.fullName =''
+  this.role =''
+  this.createdDate=''
+  this.password=''
+  this.projectName=''
+  this.refreshToken=''
+  this.refreshTokenExpiryTime=''
+    
+  }
+}
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { LoggedUser } from '../../model/User';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  loggedUserData: LoggedUser = new LoggedUser;
+  
+  router = inject(Router);
+
+  constructor(){
+    
+    const Data = localStorage.getItem('IncidentUser');
+    if(Data != null){
+      
+      this.loggedUserData = JSON.parse(Data);
+      
+    }
+  }
+
+  onLogOff(){
+
+    localStorage.removeItem('IncidentUser');
+    this.router.navigateByUrl('/login');
+  }
 }
